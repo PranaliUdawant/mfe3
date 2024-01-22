1e4a8d586b170d7fe6d669f55feff0531873ea06
Pranali Udawant <pranaliudawant15@gmail.com>
2024-01-22 19:55:44 +0530

    commit

--- micro-frontend-3/src/App.js
+++ micro-frontend-3/src/App.js
@@ -7,35 +7,36 @@ const token =
 console.log(token);
 
 const App = () => {
-  const [countryList, setcountryList] = useState(false)
+  const [countryList, setcountryList] = useState(false);
   const [viewPort, setviewPort] = useState({
     latitude: 20.5937,
     longitude: 78.9629,
     zoom: 6,
   });
- const handleCountryList = () =>{
-  setcountryList(true)
- }
+  const handleCountryList = () => {
+    setcountryList(true);
+  };
   return (
     <div>
-   {
-    countryList ? <iframe className="size-full top-0 absolute" src="http://localhost:3000" frameborder="0"></iframe> : 
-     <div
-      className="p-6 block h-screen w-4/5 ml-80 "
-    >
-    <h1 className="text-3xl text-center font-bold ">Map View</h1>
-    <a className="font-light" onClick={handleCountryList} href="#">CountryList➡️</a>
-      <ReactMapGl
-        {...viewPort}
-        mapboxAccessToken={token}
-        width="100%"
-        height="100%"
-        transitionDuration="200"
-        mapStyle="mapbox://styles/pranali-15/clq656hvd000k01pk5qvhd94m"
-      ></ReactMapGl>
+      {countryList ? (
+        <iframe src="https://mfe1-cdem.vercel.app/" frameborder="0"></iframe>
+      ) : (
+        <div className="p-6 block h-screen w-4/5 ml-80 ">
+          <h1 className="text-3xl text-center font-bold ">Map View</h1>
+          <a className="font-light" onClick={handleCountryList} href="#">
+            CountryList➡️
+          </a>
+          <ReactMapGl
+            {...viewPort}
+            mapboxAccessToken={token}
+            width="100%"
+            height="100%"
+            transitionDuration="200"
+            mapStyle="mapbox://styles/pranali-15/clq656hvd000k01pk5qvhd94m"
+          ></ReactMapGl>
+        </div>
+      )}
     </div>
-   }
-   </div>
   );
 };
 
